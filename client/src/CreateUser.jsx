aa7599153d5f1424352c8430c592ad168facf2b9
selvakumarj1807 <147826154+selvakumarj1807@users.noreply.github.com>
2024-07-15 22:08:53 +0530

    Update CreateUser.jsx

--- client/src/CreateUser.jsx
+++ client/src/CreateUser.jsx
@@ -17,48 +17,33 @@ function CreateUser() {
             })
             .catch(err => console.log(err))
     }
-    return ( <
-        div className = "d-flex vh-100 bg-primary justify-content-center align-items-center" >
-        <
-        div className = "w-50 bg-white rounded p-3" >
-        <
-        form onSubmit = { Submit } >
-        <
-        h2 > Add User < /h2> <
-        div className = "mb-2" >
-        <
-        label htmlFor = "" > Name < /label> <
-        input type = "text"
-        placeholder = "Enter Name"
-        className = "form-control"
-        onChange = {
-            (e) => setName(e.target.value) }
-        /> <
-        /div> <
-        div className = "mb-2" >
-        <
-        label htmlFor = "" > Email < /label> <
-        input type = "email"
-        placeholder = "Enter Email"
-        className = "form-control"
-        onChange = {
-            (e) => setEmail(e.target.value) }
-        /> <
-        /div> <
-        div className = "mb-2" >
-        <
-        label htmlFor = "" > Age < /label> <
-        input type = "text"
-        placeholder = "Enter Age"
-        className = "form-control"
-        onChange = {
-            (e) => setAge(e.target.value) }
-        /> <
-        /div> <
-        button className = "btn btn-success" > Submit < /button> <
-        /form> <
-        /div> <
-        /div>
+    return (
+        <div className="d-flex vh-100 bg-primary justify-content-center align-items-center">
+            <div className="w-50 bg-white rounded p-3">
+                <form onSubmit={Submit}>
+                    <h2>Add User</h2>
+                    <div className="mb-2">
+                        <label htmlFor="">Name</label>
+                        <input type="text" placeholder="Enter Name" className="form-control"
+                            onChange={(e) => setName(e.target.value)}
+                        />
+                    </div>
+                    <div className="mb-2">
+                        <label htmlFor="">Email</label>
+                        <input type="email" placeholder="Enter Email" className="form-control"
+                            onChange={(e) => setEmail(e.target.value)}
+                        />
+                    </div>
+                    <div className="mb-2">
+                        <label htmlFor="">Age</label>
+                        <input type="text" placeholder="Enter Age" className="form-control"
+                            onChange={(e) => setAge(e.target.value)}
+                        />
+                    </div>
+                    <button className="btn btn-success">Submit</button>
+                </form>
+            </div>
+        </div>
     )
 }
 
